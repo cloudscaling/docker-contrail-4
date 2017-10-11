125ec3c157b1297ff4de43cc8b23d5ace0a49418
Feodor Tersin <ftersin@hotmail.com>
2017-10-11 19:45:51 +0000

    Fix setup-k8s script

--- kubernetes/setup-k8s.sh
+++ kubernetes/setup-k8s.sh
@@ -1,26 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo service ufw stop
-sudo iptables -F
+export OHOME=$HOME
 
-sudo apt-get install \
-      docker.io \
-      apt-transport-https \
-      ca-certificates \
-      curl -y 
+sudo -u root /bin/bash << EOS
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF'
-sudo apt-get update -y
-sudo apt-get install -y kubectl
-sudo apt-get install -y kubelet
-sudo apt-get install -y kubeadm
+service ufw stop
+iptables -F
 
-sudo kubeadm init --kubernetes-version v1.7.4
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/etc/apt/sources.list.d/kubernetes.list
 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+apt-get update -y
+apt-get install -y \
+  docker.io \
+  apt-transport-https \
+  ca-certificates \
+  kubectl kubelet kubeadm
 
+kubeadm init --kubernetes-version v1.7.4 --skip-preflight-checks
+
+mkdir -p $OHOME/.kube
+cp -i /etc/kubernetes/admin.conf $OHOME/.kube/config
+chown -R $(id -u):$(id -g) $OHOME/.kube
+
+EOS
+
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+source "$DIR/../containers/parse-env.sh"
+source "$DIR/../containers/config-docker.sh"

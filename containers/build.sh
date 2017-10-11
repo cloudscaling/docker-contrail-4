f6a8d6f4de069d7f862ca4bbfe902fa86319e7aa
Feodor Tersin <ftersin@hotmail.com>
2017-10-11 23:46:33 +0000

    Optimize build

--- containers/build.sh
+++ containers/build.sh
@@ -12,25 +12,45 @@ if [ -n "$opts" ]; then
   echo 'Options: '$opts
 fi
 
+was_errors=0
+
 build () {
-  local container_name=`echo $1 | cut -d"." -f2 | tr "/" "-"`
-  local container_name=contrail${container_name}
+  local dir=$1
+  if [ ! -f ${dir}/Dockerfile ]; then
+    if [ -d ${dir}/base ]; then
+      build ${dir}/base
+    fi
+    return
+  fi
+  local container_name=`echo $dir | cut -d"." -f2 | tr "/" "-"`
+  local container_name='contrail'${container_name}
   echo 'Building '$container_name
+  local logfile='build-'$container_name'.log'
   docker build -t ${registry}'/'${container_name}:${version} \
     --build-arg CONTRAIL_VERSION=${version} \
     --build-arg CONTRAIL_REGISTRY=${registry} \
     --build-arg REPO_URL=${repository} \
-    ${opts} $1
-  docker push ${registry}'/'${container_name}:${version}
+    ${opts} $dir |& tee $logfile
+  if [ ${PIPESTATUS[0]} -eq 0 ]; then
+    docker push ${registry}'/'${container_name}:${version} |& tee -a $logfile
+    if [ ${PIPESTATUS[0]} -eq 0 ]; then
+      rm $logfile
+    fi
+  fi
+  if [ -f $logfile ]; then
+    was_errors=1
+  fi
 }
 
 if [ -z $1 ] || [ $1 = 'all' ]; then
-  for dir in `find . -type d`
-  do
-    if [ -f ${dir}/Dockerfile ]; then
+  for dir in $(find $DIR -type d); do
+    if [[ $dir != *base ]]; then
       build $dir
     fi
   done
+  if [ $was_errors -ne 0 ]; then
+    echo 'Failed to build some containers, see log files'
+  fi
 else
   build $1
 fi

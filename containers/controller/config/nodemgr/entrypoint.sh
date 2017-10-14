dc8af565442b72083c87338f4852592bfc7f2461
alexey-mr <alexey.morlang@gmail.com>
2017-10-14 21:44:51 +0300

    added conainers labels for lookup from nodemgr

--- containers/controller/config/nodemgr/entrypoint.sh
+++ containers/controller/config/nodemgr/entrypoint.sh
@@ -3,16 +3,6 @@
 CONTROLLER_NODES=${CONTROLLER_NODES:-`hostname`}
 ANALYTICS_NODES=${ANALYTICS_NODES:-${CONTROLLER_NODES}}
 
-read -r -d '' supervisord_nodemgr_config << EOM
-[program:nodemgr]
-command=/usr/bin/contrail-nodemgr --nodetype=${NODE_TYPE:-contrail-config}
-autostart=true
-autorestart=true
-redirect_stderr=true
-stdout_logfile=/dev/stdout
-stdout_logfile_maxbytes=0
-EOM
-
 analytics_server_list=''
 IFS=' ' read -ra server_list <<< "${ANALYTICS_NODES}"
 for server in "${server_list[@]}"; do
@@ -24,7 +14,13 @@ analytics_list="${analytics_server_list::-1}"
 sed -i "s/#server_list=/server_list=/g" /etc/contrail/contrail-config-nodemgr.conf
 sed -i "s/server_list=.*/server_list=${analytics_list}/g" /etc/contrail/contrail-config-nodemgr.conf
 
-echo "$supervisord_nodemgr_config" >> /etc/supervisord.conf
+if [[ -n "$DEBUG" ]] ; then
+   echo "Debug mode.."
+   while [[ ! -f '/go' ]] ; do
+     sleep 1
+   done
+   echo "Exit from debug mode"
+fi
 
 exec "$@"
 

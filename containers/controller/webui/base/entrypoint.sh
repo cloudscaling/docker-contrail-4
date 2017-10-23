07434c0eb6c5b516d640d3b0d1106da7e9f7eb38
Feodor Tersin <ftersin@hotmail.com>
2017-10-23 17:55:18 +0000

    Use common params in webui containers

--- containers/controller/webui/base/entrypoint.sh
+++ containers/controller/webui/base/entrypoint.sh
@@ -1,44 +1,14 @@
 #!/bin/bash
 
-CONTROLLER_NODES=${CONTROLLER_NODES:-`hostname`}
-ANALYTICS_NODES=${ANALYTICS_NODES:-${CONTROLLER_NODES}}
-ZOOKEEPER_NODES=${ZOOKEEPER_NODES:-${CONTROLLER_NODES}}
-CONFIG_NODES=${CONFIG_NODES:-${CONTROLLER_NODES}}
-CASSANDRA_NODES=${CASSANDRA_NODES:-${CONTROLLER_NODES}}
-RABBITMQ_NODES=${RABBITMQ_NODES:-${CONTROLLER_NODES}}
-REDIS_NODES=${REDIS_NODES:-${CONTROLLER_NODES}}
-KAFKA_NODES=${KAFKA_NODES:-${CONTROLLER_NODES}}
+source /common.sh
 
-function get_listen_ip(){
-  default_interface=`ip route show |grep "default via" |awk '{print $5}'`
-  default_ip_address=`ip address show dev $default_interface |\
-                    head -3 |tail -1 |tr "/" " " |awk '{print $2}'`
-  echo ${default_ip_address}
-}
-
-function get_server_list(){
+function get_server_json_list(){
   server_typ=$1_NODES
-  server_list=''
-  IFS=',' read -ra server_list <<< "${!server_typ}"
-  for server in "${server_list[@]}"; do
-    server_address=`echo \'${server}\'`
-    extended_server_list+=${server_address},
-  done
-  extended_list="${extended_server_list::-1}"
-  echo ${extended_list}
+  srv_list=$(echo ${!server_typ} | sed 's/,/'\',\''/g')
+  echo "['"$srv_list"']"
 }
 
-CASSANDRA_PORT=${ANALYTICS_cassandra_port:-9043}
-ZOOKEEPER_PORT=${CONFIG_zookeeoer_port:-2181}
-ANALYTICS_COLLECTOR_PORT=${ANALYTCS_COLLECTOR_analytics_port:-8086}
-ANALYTICS_API_HTTP_PORT=${ANALYTCS_API_http_port:-8090}
-ANALYTICS_API_REST_API_PORT=${ANALYTCS_API_rest_api_port:-8081}
-RABBITMQ_PORT=${CONFIG__rabbit_port:-5672}
-REDIS_PORT=${ANALYTICS_redis_port:-6379}
-KAFKA_PORT=${ALARM_GEN_kafka_port:-9092}
-CONFIG_PORT=${COLLECTOR_config_port:-8082}
-
-read -r -d '' webui_config << EOM
+cat > /etc/contrail/config.global.js << EOM
 /*
  * Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
  */
@@ -102,24 +72,24 @@ config.storageManager.strictSSL = ${storageManager_strictSSL:-false};
 config.storageManager.ca = ${storageManager_ca:-''};
 
 config.cnfg = {};
-config.cnfg.server_ip = [${cnfg_server_ip:-`get_server_list CONFIG`}];
-config.cnfg.server_port = ${cnfg_server_port:-'8082'};
+config.cnfg.server_ip = ${cnfg_server_ip:-`get_server_json_list CONFIG`};
+config.cnfg.server_port = ${cnfg_server_port:-"'"$CONFIG_API_PORT"'"};
 config.cnfg.authProtocol = ${cnfg_authProtocol:-'http'};
 config.cnfg.strictSSL = ${cnfg_strictSSL:-false};
 config.cnfg.ca = ${cnfg_ca:-''};
 config.cnfg.statusURL = ${cnfg_statusURL:-'"/global-system-configs"'};
 
 config.analytics = {};
-config.analytics.server_ip = [${analytics_server_ip:-`get_server_list ANALYTICS`}];
-config.analytics.server_port = ${analytics_server_port:-'8081'};
+config.analytics.server_ip = ${analytics_server_ip:-`get_server_json_list ANALYTICS`};
+config.analytics.server_port = ${analytics_server_port:-"'"$ANALYTICS_API_PORT"'"};
 config.analytics.authProtocol = ${analytics_authProtocol:-'http'};
 config.analytics.strictSSL = ${analytics_strictSSL:-false};
 config.analytics.ca = ${analytics_ca:-''};
 config.analytics.statusURL = ${analytics_statusURL:-'"/analytics/uves/bgp-peers"'};
 
 config.dns = {};
-config.dns.server_ip = [${dns_server_ip:-`get_server_list CONFIG`}];
-config.dns.server_port = ${dns_server_port:-'8092'};
+config.dns.server_ip = ${dns_server_ip:-`get_server_json_list CONFIG`};
+config.dns.server_port = ${dns_server_port:-"'"$DNS_INTROSPECT_PORT"'"};
 config.dns.statusURL = ${dns_statusURL:-'"/Snh_PageReq?x=AllEntries%20VdnsServersReq"'};
 
 config.vcenter = {};
@@ -142,26 +112,26 @@ config.introspect.ssl.strictSSL = false;
 
 config.jobServer = {};
 config.jobServer.server_ip = '127.0.0.1';
-config.jobServer.server_port = '3000';
+config.jobServer.server_port = '$WEBUI_JOB_SERVER_PORT';
 
 config.files = {};
 config.files.download_path = '/tmp';
 
 config.cassandra = {};
-config.cassandra.server_ips = [${cassandra_server_ips:-`get_server_list CASSANDRA`}];
-config.cassandra.server_port = ${cassandra_server_port:-'9042'};
+config.cassandra.server_ips = ${cassandra_server_ips:-`get_server_json_list CASSANDRA`};
+config.cassandra.server_port = ${cassandra_server_port:-"'"$CONFIGDB_CQL_PORT"'"};
 config.cassandra.enable_edit = ${cassandra_enable_edi:-false};
 
 config.kue = {};
-config.kue.ui_port = '3002'
+config.kue.ui_port = '$KUE_UI_PORT'
 
 config.webui_addresses = ['0.0.0.0'];
 
 config.insecure_access = false;
 
-config.http_port = '8080';
+config.http_port = '$WEBUI_HTTP_LISTEN_PORT';
 
-config.https_port = '8143';
+config.https_port = '$WEBUI_HTTPS_LISTEN_PORT';
 
 config.require_auth = false;
 
@@ -173,10 +143,10 @@ config.redisDBIndex = 3;
 
 config.CONTRAIL_SERVICE_RETRY_TIME = 300000; //5 minutes
 
-config.redis_server_port = '6379';
-config.redis_server_ip = '127.0.0.1';
+config.redis_server_port = '$REDIS_SERVER_PORT';
+config.redis_server_ip = '$REDIS_SERVER_IP';
 config.redis_dump_file = '/var/lib/redis/dump-webui.rdb';
-config.redis_password = '';
+config.redis_password = '$REDIS_SERVER_PASSWORD';
 
 config.logo_file = '/usr/src/contrail/contrail-web-core/webroot/img/opencontrail-logo.png';
 
@@ -191,7 +161,7 @@ config.qe = {};
 config.qe.enable_stat_queries = false;
 
 config.logs = {};
-config.logs.level = 'debug';
+config.logs.level = '${WEBUI_LOG_LEVEL:-debug}';
 
 config.getDomainProjectsFromApiServer = false;
 config.network = {};
@@ -202,28 +172,6 @@ config.jsonSchemaPath = "/usr/src/contrail/contrail-web-core/src/serverroot/conf
 module.exports = config;
 EOM
 
-read -r -d '' vnc_api_lib_config << EOM
-[global]
-;WEB_SERVER = 127.0.0.1
-;WEB_PORT = 9696  ; connection through quantum plugin
+set_vnc_api_lib_ini
 
-WEB_SERVER = 127.0.0.1
-WEB_PORT = ${CONFIG_api_server_port:-8082}
-BASE_URL = /
-;BASE_URL = /tenants/infra ; common-prefix for all URLs
-
-; Authentication settings (optional)
-[auth]
-;AUTHN_TYPE = keystone
-;AUTHN_PROTOCOL = http
-;AUTHN_SERVER = 127.0.0.1
-AUTHN_SERVER = ${CONFIG_AUTHN_SERVER:-""}
-;AUTHN_PORT = 35357
-;AUTHN_URL = /v2.0/tokens
-;AUTHN_TOKEN_URL = http://127.0.0.1:35357/v2.0/tokens
-EOM
-
-#get_kv
-echo "$webui_config" > /etc/contrail/config.global.js
-echo "$vnc_api_lib_config" > /etc/contrail/vnc_api_lib.ini
 exec "$@"
